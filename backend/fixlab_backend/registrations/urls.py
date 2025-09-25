52a98262e0a4a8c0c2f28fba25431d096df7517e
Fixlab Technologies Ltd <fixlabtechnologies@gmail.com>
2025-09-25 19:36:13 +0100

    Update urls.py

--- backend/fixlab_backend/registrations/urls.py
+++ backend/fixlab_backend/registrations/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import RegistrationAPIView, CheckUserAPIView,VerifyAndRegisterAPIView
+from .views import RegistrationAPIView, CheckUserAPIView,VerifyAndRegisterAPIView,HealthCheckView
+
 
 urlpatterns = [
     path('registrations/', RegistrationAPIView.as_view(), name='registrations-api'),
     path("check-user", CheckUserAPIView.as_view(), name="check-user"),
     path("api/verify-register/", VerifyAndRegisterAPIView.as_view(), name="verify-register"),
+    path('health/', HealthCheckView.as_view(), name='health-check'),
 
     
 
