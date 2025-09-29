512a381d830b28aa1410d575faae9e3d341d5e48
Fixlab Technologies Ltd <fixlabtechnologies@gmail.com>
2025-09-29 13:21:58 +0100

    Update admin.py

--- backend/fixlab_backend/registrations/admin.py
+++ backend/fixlab_backend/registrations/admin.py
@@ -14,13 +14,12 @@ class RegistrationAdmin(admin.ModelAdmin):
         'full_name',
         'email',
         'course',
-        'mode_of_learning',
         'gender',
         'payment_status',
         'reference_no',
         'created_at'
     )
-    list_filter = ('course', 'mode_of_learning', 'gender', 'payment_status')
+    list_filter = ('course', 'gender', 'payment_status')
     search_fields = ('full_name', 'email', 'phone', 'reference_no')
     readonly_fields = ('created_at',)   # ✅ allow editing reference_no if needed
 
