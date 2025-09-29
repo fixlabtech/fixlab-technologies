695c699e32625ee91fc7655eaa8b3aa79e30beab
Fixlab Technologies Ltd <fixlabtechnologies@gmail.com>
2025-09-29 15:41:00 +0100

    Update urls.py

--- backend/fixlab_backend/registrations/urls.py
+++ backend/fixlab_backend/registrations/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('registrations/', RegistrationAPIView.as_view(), name='registrations-api'),
     path("check-user", CheckUserAPIView.as_view(), name="check-user"),
     path('health/', HealthCheckView.as_view(), name='health-check'),
+    path('verify-payment/<str:reference>/', PaymentVerificationAPIView.as_view(), name='verify-payment'),
 ]
