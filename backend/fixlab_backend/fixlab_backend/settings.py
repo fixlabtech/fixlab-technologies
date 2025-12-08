ed04249aba56e9c2c2c752f9768971021b63f2de
Fixlab Technologies Ltd <fixlabtechnologies@gmail.com>
2025-12-08 16:21:46 +0100

    Update settings.py

--- backend/fixlab_backend/fixlab_backend/settings.py
+++ backend/fixlab_backend/fixlab_backend/settings.py
@@ -14,7 +14,9 @@ from pathlib import Path
 import os
 import dj_database_url
 import tempfile
-
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -196,11 +198,11 @@ REST_FRAMEWORK = {
 }
 
 
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
-}
+cloudinary.config(
+    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.environ.get("CLOUDINARY_API_KEY"),
+    api_secret=os.environ.get("CLOUDINARY_API_SECRET")
+)
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
